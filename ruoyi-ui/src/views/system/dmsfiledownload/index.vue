4420a3f74a6067ebf58b36c577b178c25506c45b
HYZ <1594479850@qq.com>
2024-01-31 18:47:18 +0800

    实现文件下载

---
+++ ruoyi-ui/src/views/system/dmsfiledownload/index.vue
@@ -0,0 +1,249 @@
+<template>
+  <div class="app-container">
+    <!--查询列表-->
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="文件ID" prop="fileId">
+        <el-input
+          v-model="queryParams.fileId"
+          placeholder="请输入文件ID"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="文件名" prop="fileName">
+        <el-input
+          v-model="queryParams.fileName"
+          placeholder="请输入文件名"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="作者" prop="author">
+        <el-input
+          v-model="queryParams.author"
+          placeholder="请输入作者"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="审稿人" prop="reviewer">
+        <el-input
+          v-model="queryParams.reviewer"
+          placeholder="请输入审稿人"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="文件类型" prop="fileType">
+        <el-select v-model="queryParams.fileType" placeholder="请选择文件类型" clearable>
+          <el-option
+            v-for="dict in dict.type.dms_file_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="文件状态" prop="fileStatus">
+        <el-select v-model="queryParams.fileStatus" placeholder="请选择文件状态" clearable>
+          <el-option
+            v-for="dict in dict.type.dms_file_status"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="归属团队" prop="belongteam">
+        <el-input
+          v-model="queryParams.belongteam"
+          placeholder="请输入归属团队"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="创建者" prop="updateBy">
+        <el-input
+          v-model="queryParams.updateBy"
+          placeholder="请输入创建者"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="创建时间" prop="updateTime">
+        <el-date-picker clearable
+          v-model="queryParams.updateTime"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="请选择创建时间">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table v-loading="loading" :data="dmsfileuploadList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="文件ID" align="center" prop="fileId" />
+      <el-table-column label="文件名" align="center" prop="fileName" />
+      <el-table-column label="文件路径" align="center" prop="filePath" />
+      <el-table-column label="作者" align="center" prop="author" />
+      <el-table-column label="审稿人" align="center" prop="reviewer" />
+      <el-table-column label="文件类型" align="center" prop="fileType">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.dms_file_type" :value="scope.row.fileType"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="文件大小" align="center" prop="fileSize" />
+      <el-table-column label="文件状态" align="center" prop="fileStatus">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.dms_file_status" :value="scope.row.fileStatus"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="归属团队" align="center" prop="belongteam" />
+      <el-table-column label="文件描述" align="center" prop="description" />
+      <el-table-column label="创建者" align="center" prop="updateBy" />
+      <el-table-column label="创建时间" align="center" prop="updateTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.updateTime, '{y}-{m}-{d} {h}:{i}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" >
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleDownload(scope.row)"
+          >下载</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+  </div>
+</template>
+
+<script>
+import { listDmsfileupload, getDmsfileupload, delDmsfileupload, addDmsfileupload, updateDmsfileupload } from "@/api/system/dmsfileupload";
+
+export default {
+  name: "Dmsfiledownload",
+  dicts: ['dms_file_type', 'dms_file_status'],
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 文件信息表格数据
+      dmsfileuploadList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        fileId: null,
+        fileName: null,
+        filePath: null,
+        author: null,
+        reviewer: null,
+        fileType: null,
+        fileSize: null,
+        fileStatus: null,
+        belongteam: null,
+        description: null,
+        updateBy: null,
+        updateTime: null
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      },
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询文件信息列表 */
+    getList() {
+      this.loading = true;
+      listDmsfileupload(this.queryParams).then(response => {
+        this.dmsfileuploadList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        fileId: null,
+        fileName: null,
+        filePath: null,
+        author: null,
+        reviewer: null,
+        fileType: null,
+        fileSize: null,
+        fileStatus: null,
+        belongteam: null,
+        description: null,
+        updateBy: null,
+        updateTime: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.fileId)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    handleDownload(row) {
+      var name = row.fileName;
+      var url = row.filePath;
+      var suffix = url.substring(url.lastIndexOf("."), url.length);
+      const a = document.createElement('a')
+      a.setAttribute('download', name + suffix)
+      a.setAttribute('target', '_blank')
+      a.setAttribute('href', url)
+      a.click()
+    },
+  },
+};
+</script>
